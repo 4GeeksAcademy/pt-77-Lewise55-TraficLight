3fccdc574902ece50fc5d4d587811210fb3c64aa
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-03-18 00:50:34 +0000

    crossWalk added

--- src/js/components/StopLight.jsx
+++ src/js/components/StopLight.jsx
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 //create your first component
 const StopLight = (props) => {
     const [selected, setSelected] = useState("redLight");
+    const [hidden, setHidden] = useState(true);
     return (
         <div className="Post">
             <div className="lightGroup m-auto">
@@ -25,6 +26,17 @@ const StopLight = (props) => {
                         onClick={() => setSelected("greenLight")}>
                     </div>
                 </div>
+                <button class="btn btn-warning mt-3" onClick={() => setSelected("hiddenLight")}></button>
+                <div className="hiddenLight mt-3">
+                    <div className={`${selected === "hiddenLight" ? "hidden light glow4 mt-3" + "crossWalk" : "" }`}>
+                         <span className="crossWalk mt-5">
+                            <i className="fas fa-walking fa-xl" ></i> 
+                         </span>                  
+                    </div>
+                    
+                    
+                                   
+                </div>               
             </div>
         </div>
     );
